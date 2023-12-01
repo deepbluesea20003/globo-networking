43323daf4bfd5cb4fddeb6675f9131b84c43ebd0
Adam.B3 <readingfootyclub1871@gmail.com>
2023-12-01 11:35:55 +0000

    Fix typo

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,7 @@ output "vpc_id"{
     description = "VPC ID"
 }
 
-output "public subnets"{
+output "public_subnets"{
     value = module.main.public_subnets
     description = "List of public subnets"
 }
